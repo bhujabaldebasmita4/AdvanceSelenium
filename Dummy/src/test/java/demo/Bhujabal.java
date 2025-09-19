d23ac0e5c65c873d20e1af40c9b148167d957f68
LONY <LONY@BHUJABAL>
2025-09-19 14:54:11 +0530

    Bhujabal commit

---
+++ Dummy/src/test/java/demo/Bhujabal.java
@@ -0,0 +1,13 @@
+package demo;
+
+public class Bhujabal {
+
+	public static void main(String[] args) {
+		System.out.println("bhujabal hello");
+		System.out.println("bhujabal hello");
+		System.out.println("bhujabal hello");
+		System.out.println("bhujabal hello");
+		System.out.println("bhujabal hello");
+	}
+
+}
